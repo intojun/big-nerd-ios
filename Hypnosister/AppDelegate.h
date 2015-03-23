ef7f12c6cc788581a75dd3a1a0e7cb48d5ffe133
infradev <infradev@infratechmbp.local>
2015-03-23 18:47:56 +0900

    Initial Commit

---
+++ Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by infradev on 2015. 3. 23..
+//  Copyright (c) 2015년 intojun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnosister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by infradev on 2015. 3. 23..
+//  Copyright (c) 2015년 intojun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
